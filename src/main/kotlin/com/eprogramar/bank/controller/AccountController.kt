8d72b85c3ffc6fe7f8772fa5865f386369f296cd
Edney Fillipi <e.fillipi@gmail.com>
2021-10-08 17:32:28 -0300

    Cors

--- src/main/kotlin/com/eprogramar/bank/controller/AccountController.kt
+++ src/main/kotlin/com/eprogramar/bank/controller/AccountController.kt
@@ -10,25 +10,30 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/accounts")
 class AccountController(private val service: AccountService) {
 
+    @CrossOrigin()
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody account: Account): Account = service.create(account)
 
+    @CrossOrigin()
     @GetMapping
     fun getAll(): List<Account> = service.getAll()
 
+    @CrossOrigin()
     @GetMapping("/{id}")
     fun getById(@PathVariable id: Long) : ResponseEntity<Account> =
             service.getById(id).map {
                 ResponseEntity.ok(it)
             }.orElse(ResponseEntity.notFound().build())
 
+    @CrossOrigin()
     @PutMapping("/{id}")
     fun update(@PathVariable id: Long, @RequestBody account: Account) : ResponseEntity<Account> =
             service.update(id, account).map {
                 ResponseEntity.ok(it)
             }.orElse(ResponseEntity.notFound().build())
 
+    @CrossOrigin()
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Long) : ResponseEntity<Void> {
         service.delete(id)
